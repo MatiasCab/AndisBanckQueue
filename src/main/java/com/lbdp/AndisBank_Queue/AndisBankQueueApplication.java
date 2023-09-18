243da838d75a3f2ebb870878a7ae5f5673ae11b9
Matias-Clavijo <matias.clavijo@koin.com.br>
2023-09-18 20:20:37 -0300

    init

---
+++ src/main/java/com/lbdp/AndisBank_Queue/AndisBankQueueApplication.java
@@ -0,0 +1,13 @@
+package com.lbdp.AndisBank_Queue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AndisBankQueueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AndisBankQueueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lbdp/AndisBank_Queue/AndisBankQueueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lbdp.AndisBank_Queue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AndisBankQueueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
